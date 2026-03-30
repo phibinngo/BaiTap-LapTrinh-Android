74dd91a429d1398a8d0c080e19eb11b3462130ff
Phi Bin Ngo <phibin120806@gmail.com>
2026-03-30 21:29:41 +0700

    Update Lab7

--- Lab7/app/build.gradle.kts
+++ Lab7/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.firebaseproject"
+        applicationId = "com.example.lab7"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
